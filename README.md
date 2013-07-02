f538cef91f0124f3a10f101ea32249df353b1f01
boo1ean <boo1ean0807@gmail.com>
2013-07-02 19:26:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Generator-yii
+[![Build Status](https://secure.travis-ci.org/boo1ean/generator-yii.png?branch=master)](https://travis-ci.org/boo1ean/generator-yii)
+
+A generator for Yeoman.
+
+## Getting started
+- Make sure you have [yo](https://github.com/yeoman/yo) installed:
+    `npm install -g yo`
+- Install the generator: `npm install -g generator-yii`
+- Run: `yo yii`
+
+## License
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ app/index.js
@@ -0,0 +1,42 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var YiiGenerator = module.exports = function YiiGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(YiiGenerator, yeoman.generators.Base);
+
+YiiGenerator.prototype.app = function app() {
+  this.mkdir('runtime');
+
+  // Public
+  this.mkdir('public');
+  this.mkdir('public/assets');
+  this.mkdir('public/css');
+
+  // Application
+  this.mkdir('app');
+  this.mkdir('app/controllers');
+  this.mkdir('app/migrations');
+  this.mkdir('app/config');
+  this.mkdir('app/models');
+  this.mkdir('app/views');
+  this.mkdir('app/behaviors');
+
+  // Tests
+  this.mkdir('tests');
+
+  this.copy('_package.json',  'package.json');
+  this.copy('_composer.json', 'composer.json');
+  this.copy('_bower.json',    'bower.json');
+};
